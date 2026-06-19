591381e4c2e31cf1521b40d5e427fa0beb2a3af4
Mpoleur <36891930+Mpoleur@users.noreply.github.com>
2026-06-20 07:03:43 +0800

    revert to V2

--- app.py
+++ app.py
@@ -5,15 +5,29 @@ import os
 
 st.title("📊 Mon Portefeuille en Direct")
 
-# On se connecte en utilisant EXACTEMENT le nom de la Resource key de Databricks
-conn = st.connection("sql-warehouse")
+# 1. Connexion automatique grâce aux variables d'environnement de Databricks Apps
+@st.cache_resource
+def get_databricks_connection():
+    return sql.connect(
+        server_hostname=os.environ.get("dbc-dfa4bdfc-bac2.cloud.databricks.com"),
+        http_path=os.environ.get("/sql/1.0/warehouses/984a09b701253a55"),
+        access_token=os.environ.get("DATABRICKS_CLIENT_TOKEN") # Token temporaire généré par l'app
+    )
 
 try:
+    # 2. Exécution de la requête SQL
     input_table = "workspace.default.portfolio_view"
     
-    # On lance la requête
-    df_portfolio = conn.query(f"SELECT * FROM {input_table}")
-
+    connection = get_databricks_connection()
+    with connection.cursor() as cursor:
+        cursor.execute(f"SELECT * FROM {input_table}")
+        result = cursor.fetchall()
+        # Récupération des noms de colonnes
+        column_names = [desc[0] for desc in cursor.description]
+    
+    # 3. Transformation en DataFrame Pandas et affichage
+    df_portfolio = pd.DataFrame(result, columns=column_names)
+    
     st.write("Voici ton portefeuille en direct de Databricks :")
     st.dataframe(df_portfolio)
 
