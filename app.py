55171f268b62176c0f28984b298f33005c16fa35
Mpoleur <matthieu.poleur@gmail.com>
2026-06-19 23:14:04 +0000

    V4

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
+import os
 import streamlit as st
 import pandas as pd
 from databricks import sql
-import os
 
 st.title("📊 Mon Portefeuille en Direct")
 
-# 1. Connexion automatique grâce aux variables d'environnement de Databricks Apps
 @st.cache_resource
 def get_databricks_connection():
+    # Databricks Apps injecte automatiquement ces variables grâce à ta ressource liée !
     return sql.connect(
-        server_hostname=os.environ.get("dbc-dfa4bdfc-bac2.cloud.databricks.com"),
-        http_path=os.environ.get("/sql/1.0/warehouses/984a09b701253a55"),
-        access_token=os.environ.get("DATABRICKS_CLIENT_TOKEN") # Token temporaire généré par l'app
+        server_hostname="dbc-bfa6bdfc-bac2.cloud.databricks.com", 
+        http_path="/sql/1.0/warehouses/dcae3895e63dfcf2",
+        access_token=os.environ.get("DATABRICKS_CLIENT_TOKEN") # <--- L'astuce est là !
     )
 
 try:
-    # 2. Exécution de la requête SQL
     input_table = "workspace.default.portfolio_view"
     
     connection = get_databricks_connection()
     with connection.cursor() as cursor:
         cursor.execute(f"SELECT * FROM {input_table}")
         result = cursor.fetchall()
-        # Récupération des noms de colonnes
         column_names = [desc[0] for desc in cursor.description]
     
-    # 3. Transformation en DataFrame Pandas et affichage
     df_portfolio = pd.DataFrame(result, columns=column_names)
     
     st.write("Voici ton portefeuille en direct de Databricks :")
