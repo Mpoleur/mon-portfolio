48097eb410eede231487665a20bb703d012f6313
Mpoleur <matthieu.poleur@gmail.com>
2026-06-20 00:31:22 +0000

    fcc

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 import os
 import streamlit as st
 import pandas as pd
-from sqlalchemy import create_engine, text
+from databricks import sql
 
-st.title("📊 Mon Portefeuille en Directm")
+st.title("📊 Mon Portefeuille en Direct")
 
 @st.cache_resource
 def get_dataframe():
-    # Adresses exactes tirées de ton écran de variables
-    host = "dbc-dfa4bdfc-bac2.cloud.databricks.com"
-    path = "/sql/1.0/warehouses/984a09b701253a55"
-    
-    # On utilise la variable système native générée automatiquement par Databricks
-    # Pas besoin de créer de variable 'DB_TOKEN', l'app possède déjà celle-ci !
-    token = os.environ.get("DATABRICKS_CLIENT_TOKEN")
-    
-    connection_url = f"databricks://token:{token}@{host}?http_path={path}"
-    engine = create_engine(connection_url)
-    
-    query = text("SELECT * FROM workspace.default.portfolio_view")
-    with engine.connect() as conn:
-        result = conn.execute(query)
-        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+    host = os.environ.get("DATABRICKS_HOST")
+    http_path = "/sql/1.0/warehouses/984a09b701253a55"
+    client_id = os.environ.get("DATABRICKS_CLIENT_ID")
+    client_secret = os.environ.get("DATABRICKS_CLIENT_SECRET")
+
+    with sql.connect(
+        server_hostname=host,
+        http_path=http_path,
+        auth_type="oauth-m2m",
+        oauth_client_id=client_id,
+        oauth_client_secret=client_secret,
+    ) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM workspace.default.portfolio_view")
+            df = cursor.fetchall_arrow().to_pandas()
     return df
 
 try:
