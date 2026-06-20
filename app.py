e8469fda64db279d98efb8ba54751134767d182e
Mpoleur <matthieu.poleur@gmail.com>
2026-06-20 04:01:28 +0000

    test 2eme db

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ import pandas as pd
 from databricks import sql
 from databricks.sdk.core import Config, oauth_service_principal
 
-#set up de la page streamlit
-st.set_page_config(
-    # Title and icon for the browser's tab bar:
-    page_title="Portfolio",
-    page_icon="📊",
-    # Make the content take up the width of the page:
-    layout="wide",
-)
-
 # routine pour chercher une table avec connection à la db
 @st.cache_resource
 def get_dataframe(selection):
@@ -37,7 +28,25 @@ def get_dataframe(selection):
             df = cursor.fetchall_arrow().to_pandas()
     return df
 
-selection = "SELECT * FROM workspace.default.portfolio_view"
-df_portfolio = get_dataframe(selection)
+#set up de la page streamlit
+st.set_page_config(
+    # Title and icon for the browser's tab bar:
+    page_title="Portfolio",
+    page_icon="📊",
+    # Make the content take up the width of the page:
+    layout="wide",
+)
+st.title("📊 Mon Portefeuille en Direct")
+
+# call portfolio view
+select1 = "select * from workspace.default.portfolio_view"
+df_portfolio = get_dataframe(select1)
 st.write("Voici ton portefeuille en direct de Databricks :")
 st.dataframe(df_portfolio)
+
+# call input
+
+select2 = "select * from workspace.default.portfolio"
+df_raw = get_dataframe(select2)
+st.write("Voici tes raw data :")
+st.dataframe(df_raw)
