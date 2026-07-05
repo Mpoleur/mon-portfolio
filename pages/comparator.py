de10ac66319971561dc558d8e5a08ce3cde08885
FourrageBantha <fourragebantha@gmail.com>
2026-07-05 18:41:27 +0800

    force int

--- pages/comparator.py
+++ pages/comparator.py
@@ -67,7 +67,7 @@ dropdown = st.selectbox(
 )
 
 df_display = df_compute[["item","store","price","unit price","measure"]][df_compute["item"]==dropdown].copy()
-df_display["price"] = df_display["price"].round(2)
+df_display["price"] = df_display["price"].astype(int)
 
 #display intput selected by dropdown
 st.dataframe(df_display.style.apply(highlight_min_row, axis=1)
